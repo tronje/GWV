ca5ac79421f6a1c161c68cd2537b2c79b109319c
tronje <hi@tron.je>
2016-11-02 12:19:05 +0100

    03: add rudimentary python script

---
+++ 03/exercise_3_2.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+def main():
+
+    # our virtual environment
+    env = []
+
+    # open file
+    with open("blatt3_environment.txt", "r") as f:
+        # list-ify each line in the file and add to env
+        for line in f:
+            env.append(list(line))
+
+    # remove newlines, we don't need 'em
+    for elem in env:
+        elem.remove('\n')
+
+        # print, to see if it worked
+        print(elem)
+
+if __name__ == '__main__':
+    main()
