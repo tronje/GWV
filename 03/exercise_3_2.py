fec87e82e66c6ff2e66826374a0d2890c74939d5
tronje <hi@tron.je>
2016-11-07 10:47:16 +0100

    oop-ify the python script

--- 03/exercise_3_2.py
+++ 03/exercise_3_2.py
@@ -1,32 +1,72 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+class PlayingField(object):
+    """Represents a playing field, which is read
+    from a file and stored in a 2d-list.
+    Can be printed and (TODO) searched through.
+    """
+
+    def __init__(self, filename):
+        """Initialize a playing field.
+
+        Params:
+        -------
+        filename : str
+            The name of the file from which to read
+            the playing field.
+        """
+
+        # name of the file to use
+        self._filename = filename
+
+        # our virtual environment
+        self._environment = []
+
+        # read env from the file
+        with open(filename, "r") as f:
+            for line in f:
+                self._environment.append(list(line))
+
+        # remove newlines, they're not relevant to us
+        for elem in self._environment:
+            elem.remove('\n')
+
+    def __str__(self):
+        """Print our env prettily.
+        """
+
+        ret = ""
+        for line in self.environment:
+            for char in line:
+                ret += char
+            ret += '\n'
+
+        return ret
+
+    @property
+    def environment(self):
+        """Environment property.
+        """
+
+        return self._environment
+
+    @property
+    def env(self):
+        """Shorthand for environment property.
+        """
+
+        return self._environment
+
 def main():
+    # create playing field object from the txt file
+    field = PlayingField("blatt3_environment.txt")
     # print, to see if it worked
-    env = read_env()
-    print_env(env)
+    print(field)
 
-def read_env():
-    # our virtual environment
-    env = []
-
-    # open file
-    with open("blatt3_environment.txt", "r") as f:
-        # list-ify each line in the file and add to env
-        for line in f:
-            env.append(list(line))
-
-    # remove newlines, we don't need 'em
-    for elem in env:
-        elem.remove('\n')
-
-    return env
-
-def print_env(env):
-    for line in env:
-        for char in line:
-            print(char, end="")
-        print("")
+    # try the other file as well
+    field = PlayingField("blatt3_environment_b.txt")
+    print(field)
 
 if __name__ == '__main__':
     main()
