b2ad603d405b46958a2b1db024f376a1cf99d5d9
tronje <hi@tron.je>
2016-11-14 10:50:59 +0100

    make main.py more usable and verbose

--- 04/main.py
+++ 04/main.py
@@ -5,11 +5,21 @@ import sys
 import searching
 from playing_field import PlayingField
 
-def main(filename):
+def main(filename, sfunc):
     field = PlayingField(filename)
     print("Start: " + str(field.findStartNode()))
     print(field)
-    field.search(searching.bfs)
+    field.search(sfunc)
 
 if __name__ == "__main__":
-    main(sys.argv[1])
+    if len(sys.argv) != 3:
+        print("Usage: ./main.py <filename> <search-strategy>")
+        sys.exit(1)
+
+    try:
+        sfunc = searching.searches[sys.argv[2]]
+    except KeyError:
+        print("search strategy '{}' not supported!".format(sys.argv[2]))
+
+    main(sys.argv[1], sfunc)
+    sys.exit(0)
