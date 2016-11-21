7acf4fba27af5ef9ecf1d8f48f5ca71d34df4571
tronje <hi@tron.je>
2016-11-21 10:02:11 +0100

    add some comments to explain what's happening in make_archive script

--- make_archive.sh
+++ make_archive.sh
@@ -1,19 +1,29 @@
 #!/bin/bash
 
+# expect exactly one argument
 if [ $# -ne 1 ]; then
     echo "Invalid arguments!"
     exit 1
 fi
 
+# name starts with first (and only) argument!
 name=$1_04_Joergensen_Wille_Krabbe
+
+# copy the directory with the given name
+# to the newly generated name
 cp -r $1 $name
 
 echo "Creating tar ball..."
+
+# tar it, excluding all files listed in archive_exclude.txt
 tar -X archive_exclude.txt -cvf $name.tar $name
 
+# remove the directory, we no longer need it
 rm -r $name
 
 echo "Gzipping tar ball..."
+
+# gzip -9 for maximum compression
 gzip -9 -f $name.tar
 
 echo "Created $name.tar.gz"
